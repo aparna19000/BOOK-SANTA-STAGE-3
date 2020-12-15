751b1860a8b09d5106b48a3e7f556d01bc5809f9
aparna19000 <aparna19000@gmail.com>
2020-12-16 01:05:43 +0530

    new files

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -64,7 +64,7 @@ export default class WelcomeScreen extends Component{
 userLogin = (emailId, password)=>{
    firebase.auth().signInWithEmailAndPassword(emailId, password)
    .then(()=>{
-     return Alert.alert("Successfully Login")
+    this.props.navigation.navigate('DonateBooks')
    })
    .catch((error)=> {
      var errorCode = error.code;
