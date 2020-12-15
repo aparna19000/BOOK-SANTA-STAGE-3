7919799c79dc5a4fff91bd83b9db69f93cfeb842
aparna19000 <aparna19000@gmail.com>
2020-12-16 01:00:11 +0530

    tab added

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import WelcomeScreen from './screens/WelcomeScreen';
+import { AppTabNavigator } from './components/AppTabNavigator'
 
 export default function App() {
   return (
-    <WelcomeScreen/>
+    <AppContainer/>
   );
 }
+
+
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen:{screen: WelcomeScreen},
+  BottomTab:{screen: AppTabNavigator}
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
